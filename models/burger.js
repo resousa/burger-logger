2116086e54392d0e91b1a66831c0af526930e523
Ryan Sousa <Sousa.Ryan4@gmail.com>
2020-03-02 07:13:27 -0800

    modles added

---
+++ models/burger.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const orm = require("../config/orm.js");
+
+const burger = {
+  all: function(cb) {
+    orm.all("burgers", res => {
+      cb(res);
+    });
+  },
+
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, res => {
+      cb(res);
+    });
+  },
+
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, res => {
+      cb(res);
+    });
+  },
+
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, res => {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
