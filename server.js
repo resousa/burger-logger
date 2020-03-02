ed81e10111949e86a12353a38bdd20a3ce625446
Ryan Sousa <Sousa.Ryan4@gmail.com>
2020-03-02 00:22:02 -0800

    added server

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const expressHandlebars = require('express-handlebars');
+
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+const routes = require('./controllers/burgerController.js');
+
+app.use(routes);
+
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
